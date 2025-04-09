9a278cfb3a422f4f4df0bbe1ab1cf70b5a7043d5
DESKTOP-KCR9RD5\정현 <kastor0323@gmail.com>
2025-04-09 22:42:23 +0900

    back)db테이블 생성 및 문제 해결 완료

--- Back/Spring/Reblancing/src/main/java/com/upwardright/reblancing/ReblancingApplication.java
+++ Back/Spring/Reblancing/src/main/java/com/upwardright/reblancing/ReblancingApplication.java
@@ -3,7 +3,6 @@ package com.upwardright.reblancing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//exclude는 추후 삭제
 @SpringBootApplication
 public class ReblancingApplication {
 
